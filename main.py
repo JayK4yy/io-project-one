fbbcbf57bb6594bf7f70872c80c9ee8f3ce0e4e3
JayK4yy <jayk4yy@gmail.com>
2021-05-25 11:26:58 +0200

    zmiany w main.py

--- main.py
+++ main.py
@@ -12,23 +12,28 @@ def main():
 
 def podajpracownika():
     dane = input_group("Dodawanie pracownika", [
-        input('Imię', name='imie'),
-        input('Nazwisko', name='nazwisko'),
-        input('Data urodzenia', name='data_urodzenia'),
-        radio('Płeć', options=['M', 'K'], name='plec'),
-        input('Numer telefonu', type=NUMBER, name='phone'),
+        input('Imię', name='firstName'),
+        input('Nazwisko', name='lastName'),
         input('Adres e-mail', name='email'),
-        input('Miasto', name='miasto'),
-        input('Kod pocztowy', name='postcode'),
-        input('Ulica', name='ulica'),
+        radio('Płeć', options=['M', 'K'], name='gender'),
+        input('Numer telefonu', type=NUMBER, name='phone'),
+        input('Ulica', name='adress'),
+        input('Kod pocztowy', name='postalCode'),
+        input('Miasto', name='city'),
+        input('Stanowisko', name='jobTitle'),
+        input('Biuro', name='office'),
+        input('Przełożony', name='reportsTo'),
+
+        # umiejętności
         radio('C', options=['Tak', 'Nie'], name='C'),
         radio('C++', options=['Tak', 'Nie'], name='Cpp'),
         radio('Python', options=['Tak', 'Nie'], name='Python'),
         radio('Java', options=['Tak', 'Nie'], name='Java')
     ])
-    output('aaa')
     put_text(dane)
 
 
 if __name__ == '__main__':
     podajpracownika()
+
+
