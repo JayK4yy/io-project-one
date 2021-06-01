1264e4cf3c9f4b04cafba29cf390eda31cab0515
Kacper Kubicki <82718318+Xaness@users.noreply.github.com>
2021-06-01 11:36:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import mariadb
 from pywebio.input import *
 from pywebio.output import *
 from login import *
-
+from addEmployee import *
 
 def main():
     clear()
@@ -19,88 +19,6 @@ def main():
     elif mainmenu['action'] == 'logout':
         print('Wylogowano')
         exit()
-
-
-def addEmployee():
-    def addSQL(data):
-        #Tutaj tylko wartosci zmieniamy zeby nie bylo Tak/Nie
-        skills=['C','Cpp','Cs','Python','Java','HTML','CSS','JavaScript','SQL','PHP']
-
-        for skill in skills:
-            if data[skill]=='Tak':
-                data[skill]=1
-            elif data[skill]=='Nie':
-                data[skill]=0
-
-        #Laczymy sie z baza(przyda sie zrobic do tego funckje)
-        conn = mariadb.connect(
-            user='projectOneUser',
-            password='VeryHardP@ssw0rd',
-            host="localhost",
-            port=3306,
-            database="ioProjectOne"
-        )
-
-
-        cursor = conn.cursor()
-        cursor.execute("SELECT MAX(employeeNumber) FROM employees")
-        userID=cursor.fetchall()[0][0]+1
-
-        #Dodajemy do pracownikow
-        query_user="""INSERT INTO employees(employeeNumber,lastName,firstName,office,jobTitle,city,postalCode,adress,phone,gender,email)
-                       VALUES (%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
-        values_user=(userID,data['lastName'],data['firstName'],data['office'],data['jobTitle'],data['city'],data['postalCode'],data['adress'],data['phone'],
-                data['gender'],data['email'])
-        cursor.execute(query_user, values_user)
-
-       #Dodajemy do skilli
-        query_skills = """INSERT INTO skills(employeeNumber,c,html,css,java,python,javascript,cs,sqlskill,php,cpp) VALUES (%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d)"""
-        values_skills = (
-            userID,data['C'],  data['Python'], data['Java'], data['HTML'], data['CSS'],data['JavaScript'],data['Cs'],data['SQL'],data['PHP'],data['Cpp'])
-        cursor.execute(query_skills, values_skills)
-
-        conn.commit()
-
-
-
-
-    data = input_group("Dodawanie pracownika", [
-        input('Imię', name='firstName'),
-        input('Nazwisko', name='lastName'),
-        input('Adres e-mail', name='email'),
-        radio('Płeć', options=['M', 'K'], name='gender'),
-        input('Numer telefonu', name='phone'),
-        input('Ulica', name='adress'),
-        input('Kod pocztowy', name='postalCode'),
-        input('Miasto', name='city'),
-        input('Stanowisko', name='jobTitle'),
-        input('Biuro', name='office'),
-        input('Przełożony', name='reportsTo'),
-        input('Hasło', name='password'),
-
-        # umiejętności
-        radio('C', options=['Tak', 'Nie'], name='C'),
-        radio('C++', options=['Tak', 'Nie'], name='Cpp'),
-        radio('C#', options=['Tak', 'Nie'], name='Cs'),
-        radio('Python', options=['Tak', 'Nie'], name='Python'),
-        radio('Java', options=['Tak', 'Nie'], name='Java'),
-        radio('HTML', options=['Tak', 'Nie'], name='HTML'),
-        radio('CSS', options=['Tak', 'Nie'], name='CSS'),
-        radio('JavaScript', options=['Tak', 'Nie'], name='JavaScript'),
-        radio('SQL', options=['Tak', 'Nie'], name='SQL'),
-        radio('PHP', options=['Tak', 'Nie'], name='PHP'),
-
-        actions('', [
-            {'label': 'Zapisz', 'value': 'save'},
-            {'label': 'Anuluj', 'value': 'cancel'}
-        ], name='action'),
-    ])
-    if data['action'] == 'save':
-        addSQL(data)
-        main()
-    elif data['action'] == 'cancel':
-        main()
-
 
 def login():
 
@@ -138,3 +56,4 @@ def login():
 if __name__ == '__main__':
     login()
     main()
+    #addEmployee()
